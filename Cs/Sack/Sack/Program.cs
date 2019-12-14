3cd25b4374682d69e117ca5c1ab9414026365b65
Alex Chlabicz <alexchlabicz@gmail.com>
2019-12-14 22:04:15 +1100

    initial commit

---
+++ Cs/Sack/Sack/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sack testSack = new Sack();
+            testSack.Add(3);
+            testSack.Add("hello");
+            testSack.Add(false);
+            var test = testSack.Retrieve();
+            Console.WriteLine(test);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cs/Sack/Sack/Sack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sack
+{
+    public class Sack
+    {
+        private List<object> objects;
+
+        public Sack()
+        {
+            objects = new List<object>();
+        }
+
+        public void Add<T>(T obj)
+        {
+            objects.Add(obj);
+        }
+
+        public object Retrieve()
+        {
+            int numberObjects = objects.Count;
+
+            Random rand = new Random();
+            int selectedIndex = rand.Next(0, numberObjects);
+
+            return objects[selectedIndex];
+        }
+
+        public object Remove()
+        {
+            int numberObjects = objects.Count;
+
+            Random rand = new Random();
+            int selectedIndex = rand.Next(0, numberObjects);
+
+            object selectedObject = objects[selectedIndex];
+            objects.RemoveAt(selectedIndex);
+
+            return selectedObject;
+        }
+
+        public void Empty()
+        {
+            objects.Clear();
+        }
+    }
+}
